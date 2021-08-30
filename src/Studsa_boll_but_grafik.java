c8a45f93c4ba652b706a1d04c73b68e14f416a9e
Snubb <60389050+Snubb@users.noreply.github.com>
2021-08-30 09:53:08 +0200

    Lade till grafik så man kan se boll studsa :)

--- src/Studsa_boll_but_grafik.java
+++ src/Studsa_boll_but_grafik.java
@@ -1,9 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferStrategy;
 
 public class Studsa_boll_but_grafik extends Canvas implements Runnable {
 
-    private final int width = 600; //Dimensions for playing area
+    private final int width = 600;
     private final int height = 600;
 
     private Thread thread;
@@ -11,6 +12,14 @@ public class Studsa_boll_but_grafik extends Canvas implements Runnable {
     int fps = 60;
 
     private boolean isRunning;
+
+    private BufferStrategy bs;
+
+    double ballPosX = 10;
+    double ballPosY = 50;
+    double ballSpeedX = 5;
+    double ballSpeedY = 0;
+    private static final double gravity = 1;
 
     public Studsa_boll_but_grafik() {
 
@@ -61,10 +70,47 @@ public class Studsa_boll_but_grafik extends Canvas implements Runnable {
     }
 
     private void draw() {
+        bs = getBufferStrategy();
+        if (bs == null) {
+            createBufferStrategy(3);
+            return;
+        }
 
+        Graphics g = bs.getDrawGraphics();
+
+        g.setColor(Color.white);
+        g.fillRect(0,0,width,height);
+
+        g.setColor(Color.black);
+
+        g.fillOval((int)Math.round(ballPosX) - 5, (int)Math.round(ballPosY) - 5, 5, 5);
+
+        g.dispose();
+        bs.show();
     }
 
     private void update() {
+        ballPosX += ballSpeedX;
+        ballSpeedY += gravity/10;
+        ballPosY += ballSpeedY;
+        if (ballPosY >= 600) {
+            ballPosY = ballPosY - (ballPosY - 600) * 2;
+            ballSpeedY = (ballSpeedY * -1)*0.9;
+            if (ballSpeedX > 0.05) {
+                ballSpeedX -= 0.05;
+            } else if (ballSpeedX < -0.05) {
+                ballSpeedX += 0.05;
+            } else {
+                ballSpeedX = 0;
+                ballSpeedY = 0;
+            }
+        }
+        if (ballPosX >= 600) {
+            ballSpeedX = (ballSpeedX * -1)*0.9;
+        }
+        if (ballPosX <= 5) {
+            ballSpeedX = (ballSpeedX * -1)*0.9;
+        }
 
     }
 }
